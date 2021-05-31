944f6b0170a6dc394e642048db36fc2a23d19af0
Hyun-Jin Won <ohj8447@gmail.com>
2021-05-31 23:38:36 +0900

    Rename ft_format to ft_format_decode.c
    ft_format_decode define format decode
    decode data will be saved by struct t_format_info
    each print function will be print used by t_format_info

--- ft_format_decode.c
+++ ft_format_decode.c
@@ -1,15 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_format.c                                        :+:      :+:    :+:   */
+/*   ft_format_decode.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hwon <ohj8447@gmail.com>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 19:19:32 by hwon              #+#    #+#             */
-/*   Updated: 2021/05/31 22:37:31 by hwon             ###   ########.fr       */
+/*   Updated: 2021/05/31 23:37:44 by hwon             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-	
+t_format_info	*ft_format_decode(const char *str)
+{
+	t_format_info *info;
+
+	info = ft_create_info();
+	if (!info)
+		return (0);
+	while (!*str)
+	{
+		if (ft_istype(*str))
+		{
+			check_type(str, info);
+			break;
+		}
+		str = check_optino(str, info);
+	}
+	return  (info);
+}	
