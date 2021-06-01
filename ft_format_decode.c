29df8f737ed290d16d4208d5acb1521865b0977c
Hyun-Jin Won <ohj8447@gmail.com>
2021-06-01 22:19:41 +0900

    Add check_type check_option

--- ft_format_decode.c
+++ ft_format_decode.c
@@ -6,27 +6,59 @@
 /*   By: hwon <ohj8447@gmail.com>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/31 19:19:32 by hwon              #+#    #+#             */
-/*   Updated: 2021/05/31 23:37:44 by hwon             ###   ########.fr       */
+/*   Updated: 2021/06/01 22:18:51 by hwon             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+bool	istype(int c)
+{
+	if (c == 'c' || c == 's' || c == 'p' || c == 'd' || c == 'i' || c =='u')
+		return (true);
+	if (c == 'x' || c == 'X' || c == '%')
+		return (true);
+	return (false);
+}
+
+bool	check_type(int c, t_format_info *info)
+{
+	if (!info && !(*info))
+		return (false);
+	if (!istype(*str))
+		return (false);
+	info->type = *str;
+	return (true);
+}
+
+char	*check_option(const char *str, t_format_info *info)
+{
+	int tmp;
+
+	if (*str == '-')
+		info->align = RIGHT;
+	else if (*str == '0')
+		info->filled = '0';
+	else if (*str == '*')
+		info->width = 0; //TODO: get arg value
+	return (str++);
+}
+
 t_format_info	*ft_format_decode(const char *str)
 {
 	t_format_info *info;
 
-	info = ft_create_info();
-	if (!info)
-		return (0);
-	while (!*str)
+	info = ft_create_format_info();
+	if (!info || !str)
 	{
-		if (ft_istype(*str))
-		{
-			check_type(str, info);
+		ft_delete_format_info(info);
+		return (0);
+	}
+	while (*str)
+	{
+		if (check_type(*str, info))
 			break;
-		}
-		str = check_optino(str, info);
+		str = check_option(str, info);
 	}
 	return  (info);
-}	
+}
