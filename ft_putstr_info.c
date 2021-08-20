3f0e76278dbace1f72c57c7fb374a6b7977ca308
HyunJin Won <ohj8447@gmail.com>
2021-08-20 19:40:04 +0900

    add printf %s func if str_pointer is null)

--- ft_putstr_info.c
+++ ft_putstr_info.c
@@ -6,7 +6,7 @@
 /*   By: hwon <ohj8447@gmail.com>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/03 14:42:26 by hwon              #+#    #+#             */
-/*   Updated: 2021/07/26 19:34:06 by hwon             ###   ########.fr       */
+/*   Updated: 2021/08/20 19:39:29 by hwon             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,7 @@ int	ft_putstr_info(t_format_info *info, va_list *va)
 	if (info->type != 's')
 		return (0);
 	str = va_arg(*va, char *);
+	if (str == 0)
+		return (write(1, "(null)", 6));
 	return (write(1, str, ft_strlen(str)));
 }
